34288aae54e775fd6ff8e116a3a8965231e89160
PaytonPerchez <49881436+PaytonPerchez@users.noreply.github.com>
2023-03-08 23:09:29 -0800

    Create Smt_Dungeon.java and Update README.md
    Created the Java file we will use to reimplement Whitehead's code. Updated the README file to include the purpose of this repository.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS-5990_PCG_Project
+Java implementation of Jim Whitehead's procedural dungeon generation algorithm.
+* [Whitehead's code](https://github.com/JimWhiteheadUCSC/smt_dungeon)
+* [Whitehead's paper](https://dl.acm.org/doi/10.1145/3402942.3409603)
---
+++ Smt_Dungeon.java
@@ -0,0 +1,15 @@
+package project;
+
+/**
+ * Java implementation of Jim Whitehead's procedural dungeon generation algorithm
+ * Source: (https://github.com/JimWhiteheadUCSC/smt_dungeon)
+ * @author Jack Peabody
+ * @author Payton Perchez
+ */
+public class Smt_Dungeon {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
