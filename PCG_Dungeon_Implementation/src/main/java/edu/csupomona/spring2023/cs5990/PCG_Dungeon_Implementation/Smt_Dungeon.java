d30144a9b256800bab5d00304e947585c6acca0c
PaytonPerchez <49881436+PaytonPerchez@users.noreply.github.com>
2023-05-07 16:50:14 -0700

    Implemented drawPassageways
    Implemented the drawPassageways method as well as required methods rangeOverlapping and overlap. Replaced Polygon objects in drawRooms with Polyline objects.

--- PCG_Dungeon_Implementation/src/main/java/edu/csupomona/spring2023/cs5990/PCG_Dungeon_Implementation/Smt_Dungeon.java
+++ PCG_Dungeon_Implementation/src/main/java/edu/csupomona/spring2023/cs5990/PCG_Dungeon_Implementation/Smt_Dungeon.java
@@ -6,14 +6,14 @@ import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Polyline;
 import javafx.scene.control.Button;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-//import java.util.Random;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 //import javax.print.attribute.standard.NumberOfInterveningJobs;
 
@@ -280,13 +280,13 @@ public class Smt_Dungeon extends Application
 				// Default color of rectangle is already black
 				break;
 			case 2:
-				rectangle.setFill(Color.ORANGE);
+				rectangle.setFill(Color.rgb(255, 133, 27));	// orange
 				break;
 			case 3:
-				rectangle.setFill(Color.BLUE);
+				rectangle.setFill(Color.rgb(0, 116, 217));	// blue
 				break;
 			case 4:
-				rectangle.setFill(Color.GREEN);
+				rectangle.setFill(Color.rgb(46, 204, 64));	// green
 				break;
 			default:
 				break;
@@ -298,21 +298,26 @@ public class Smt_Dungeon extends Application
 		{
 			if(showDelaunay)
 			{
+				Polyline lines = new Polyline();
+				lines.setStroke(Color.rgb(0, 45, 225));	// dark blue
+				
 				// Draw Delaunay triangulation
 				for(Triangle2D triangle : tri.getTriangles())
 				{
 					//System.out.println("t is " + triangle);
-					Polygon lines = new Polygon();
 					// TODO convert points to int??
 					lines.getPoints().addAll(
 						(triangle.a.x + BORDER), (triangle.a.y / SCALE_FACTOR) + BORDER,
 						(triangle.b.x + BORDER), (triangle.b.y / SCALE_FACTOR) + BORDER,
 						(triangle.c.x + BORDER), (triangle.c.y / SCALE_FACTOR) + BORDER
 					);
-					lines.setFill(null);
-					lines.setStroke(Color.DARKBLUE);
-//					SOMENODE.getChildren().add(lines);
 				}
+				
+				lines.getPoints().addAll(
+					lines.getPoints().get(0),
+					lines.getPoints().get(1)
+				);
+//				SOMENODE.getChildren().add(lines);
 			}
 			
 		}// end if
@@ -321,9 +326,8 @@ public class Smt_Dungeon extends Application
 		{
 			if(showSparse)
 			{
-				Polygon lines = new Polygon();
-				lines.setFill(null);
-				lines.setStroke(Color.LIMEGREEN);
+				Polyline lines = new Polyline();
+				lines.setStroke(Color.rgb(0, 225, 0));	// lime green
 				// TODO convert points to int??
 				for(int[] points : mst)
 				{
@@ -334,14 +338,18 @@ public class Smt_Dungeon extends Application
 						(double) (centerPoints[points[1]][2] / SCALE_FACTOR) + BORDER
 					);
 				}
+				lines.getPoints().addAll(
+					lines.getPoints().get(0),
+					lines.getPoints().get(1)
+				);
 //				SOMENODE.getChildren().add(lines);
 			}
 		}
 		
 		if(!mousePoints.isEmpty())
 		{
-			Polygon lines = new Polygon();
-			lines.setFill(null);
+			Polyline lines = new Polyline();
+			lines.setStroke(Color.rgb(139, 0, 0));	// dark red
 			for(double[] points : mousePoints)
 			{
 				lines.getPoints().addAll(points[0], points[1]);
@@ -349,7 +357,156 @@ public class Smt_Dungeon extends Application
 //			SOMENODE.getChildren().add(lines);
 		}
 		
+		drawPassageways(m, mst);
+		
 	}// end drawRooms
+	
+	
+	private void drawPassageways(Model m, int[][] mst)
+	{
+		Random random = new Random();
+		int top;
+		int bottom;
+		int left;
+		int right;
+		int[] overlap;
+		int passX;
+		int passY;
+		
+		for(int[] points : mst)
+		{
+			// Determine which room is above the other room
+			if(Integer.parseInt(m.getConstInterp(rooms.get(points[0]).getY()).toString()) < Integer.parseInt(m.getConstInterp(rooms.get(points[1]).getY()).toString()))
+			{
+				top = points[0];
+				bottom = points[1];
+				
+			} else {
+				
+				top = points[1];
+				bottom = points[0];
+			}
+			
+			// Determine which room is to right of other room
+			if(Integer.parseInt(m.getConstInterp(rooms.get(points[0]).getX()).toString()) < Integer.parseInt(m.getConstInterp(rooms.get(points[1]).getX()).toString()))
+			{
+				left = points[0];
+				right = points[1];
+				
+			} else {
+				
+				right = points[0];
+				left = points[1];
+			}
+			
+			int[] top_x_range = new int[] {Integer.parseInt(m.getConstInterp(rooms.get(top).getX()).toString()) + PASSAGE_WIDTH, Integer.parseInt(m.getConstInterp(rooms.get(top).getX()).toString()) + rooms.get(top).getWidth() - PASSAGE_WIDTH};
+			int[] top_y_range = new int[] {Integer.parseInt(m.getConstInterp(rooms.get(top).getY()).toString()) + PASSAGE_WIDTH, Integer.parseInt(m.getConstInterp(rooms.get(top).getY()).toString()) + rooms.get(top).getHeight() - PASSAGE_WIDTH};
+			int[] bottom_x_range = new int[] {Integer.parseInt(m.getConstInterp(rooms.get(bottom).getX()).toString()) + PASSAGE_WIDTH, Integer.parseInt(m.getConstInterp(rooms.get(bottom).getX()).toString()) + rooms.get(bottom).getWidth() - PASSAGE_WIDTH};
+			int[] bottom_y_range = new int[] {Integer.parseInt(m.getConstInterp(rooms.get(bottom).getY()).toString()) + PASSAGE_WIDTH, Integer.parseInt(m.getConstInterp(rooms.get(bottom).getY()).toString()) + rooms.get(bottom).getHeight() - PASSAGE_WIDTH};
+			
+			Polyline lines = new Polyline();
+			lines.setStrokeWidth(PASSAGE_WIDTH);
+			
+			if(rangeOverlapping(top_x_range, bottom_x_range))
+			{
+				if(rangeOverlapping(top_y_range, bottom_y_range))
+				{
+					System.out.println("Rooms overlapping??");
+					
+				} else {
+					
+					// x overlap, no y overlap. Drop passage down from top room to bottom room
+					overlap = overlap(top_x_range, bottom_x_range);
+					passX = overlap[random.nextInt(overlap.length)];
+					lines.getPoints().addAll(
+						(double) passX + BORDER, (double) (((Integer.parseInt(m.getConstInterp(rooms.get(top).getY()).toString()) + rooms.get(top).getHeight()) / SCALE_FACTOR) + BORDER),
+						(double) passX + BORDER, (double) ((Integer.parseInt(m.getConstInterp(rooms.get(bottom).getY()).toString()) / SCALE_FACTOR) + BORDER)
+					);
+					
+//					SOMENODE.getChildren().add(lines);
+				}
+				
+			} else {
+				
+				if(rangeOverlapping(top_y_range, bottom_y_range))
+				{
+					// y overlap, no x overlap, draw line straight across
+					overlap = overlap(top_y_range, bottom_y_range);
+					passY = overlap[random.nextInt(overlap.length)];
+					lines.getPoints().addAll(
+						(double) (Integer.parseInt(m.getConstInterp(rooms.get(left).getX()).toString()) + rooms.get(left).getWidth() + BORDER), (double) ((passY / SCALE_FACTOR) + BORDER),
+						(double) (Integer.parseInt(m.getConstInterp(rooms.get(right).getX()).toString()) + BORDER), (double) ((passY / SCALE_FACTOR) + BORDER)
+					);
+					
+//					SOMENODE.getChildren().add(lines);
+					
+				} else {
+					
+					// no x overlap, no y overlap, draw right-angle connector
+					passX = random.nextInt(bottom_x_range[1] - bottom_x_range[0]) + bottom_x_range[0];
+					passY = random.nextInt(top_y_range[1] - top_y_range[0]) + top_y_range[0];
+					if(top == left)
+					{
+						lines.getPoints().addAll(
+							(double) (Integer.parseInt(m.getConstInterp(rooms.get(top).getX()).toString()) + rooms.get(top).getWidth() + BORDER), (double) ((passY / SCALE_FACTOR) + BORDER)
+						);
+						
+					} else {
+						
+						lines.getPoints().addAll(
+							(double) (Integer.parseInt(m.getConstInterp(rooms.get(top).getX()).toString()) + BORDER),
+							(double) ((passY / SCALE_FACTOR) + BORDER)
+						);
+					}
+					
+					lines.getPoints().addAll(
+						(double) (passX + BORDER + (PASSAGE_WIDTH / 2)), (double) ((passY / SCALE_FACTOR) + BORDER)
+					);
+					
+//					SOMENODE.getChildren().add(lines);
+					lines = new Polyline();
+					lines.setStrokeWidth(PASSAGE_WIDTH);
+					
+					lines.getPoints().addAll(
+						(double) (passX + BORDER), (double) ((passY / SCALE_FACTOR) + BORDER),
+						(double) (passX + BORDER), (double) (Integer.parseInt(m.getConstInterp(rooms.get(bottom).getY()).toString()) / SCALE_FACTOR)
+					);
+//					SOMENODE.getChildren().add(lines);
+					
+				}// end if
+				
+			}// end if
+			
+		}// end for
+		
+	}// end drawPassageways
+	
+	
+	private boolean rangeOverlapping(int[] range1, int[] range2)
+	{
+		if((range1[0] == range1[1]) || (range2[0] == range2[1]))
+		{
+			return false;
+			
+		} else {
+			
+			return (((range1[0] < range2[1]) && (range1[1] > range2[0])) ||
+					((range1[1] > range2[0]) && (range2[1] > range1[0])));
+		}
+	}
+	
+	
+	private int[] overlap(int[] range1, int[] range2)
+	{
+		if(!rangeOverlapping(range1, range2))
+		{
+			return new int[] {};
+			
+		} else {
+			
+			return IntStream.rangeClosed(Math.max(range1[0], range2[0]), Math.min(range1[1], range2[1]) + 1).toArray();
+		}
+	}
 	
 	/**
 	 * Computes the distance between the two given points.
